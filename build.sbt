3ad74b379a9738ede3b399e539934a655828aa19
Wondaa3 <jasmin.tran2002@gmail.com>
2026-04-16 13:04:47 +0200

    build for tests, begining PlayerSpec

--- build.sbt
+++ build.sbt
@@ -2,15 +2,13 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "3.3.7"
 
-libraryDependencies += "org.scalactic" %% "scalactic" % "3.2.14"
-libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.14" % "test"
-
 lazy val root = (project in file("."))
   .settings(
     name := "Konstopoly",
     idePackagePrefix := Some("de.konstopoly"),
     libraryDependencies ++= Seq(
       "org.scala-lang.modules" %% "scala-swing" % "3.0.0",
+      "org.scalactic"          %% "scalactic"   % "3.2.18",
       "org.scalatest"          %% "scalatest"   % "3.2.18" % Test
     )
   )
--- src/test/scala/de/konstopoly/model/PlayerSpec.scala
+++ src/test/scala/de/konstopoly/model/PlayerSpec.scala
@@ -1,8 +1,42 @@
 package de.konstopoly.model
 
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers._
+
 // Tests für die Player-Klasse.
 // Prüft: Initialisierung mit Name/Startkapital, Bewegung auf dem Brett,
 // Geld hinzufügen/abziehen und Grundstücke kaufen/besitzen.
-class PlayerSpec {
+class PlayerSpec extends AnyWordSpec{
+  "A Player" when {
 
+    //initialiesierung
+    "created with name and starting capital" should {
+
+      val player = Player("Jasmin", 1500)
+
+      "have the correct name" in {
+        player.name should be ("Jasmin")
+      }
+
+      "start with 1500 money" in {
+        player.money should be ("1500")
+      }
+
+      "start at position 0" in {
+        player.position should be (0)
+      }
+
+      "start with no properties" in {
+        player.properties should be (0)
+      }
+      
+      "start with selected figure" in {
+        player.figure
+      }
+    }
+
+    //bewegung
+
+    
+  }
 }
