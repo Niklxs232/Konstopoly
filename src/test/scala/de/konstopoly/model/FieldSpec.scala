ed6969d710edff17dd75b7531a36fa30467d0082
niklassarbasini <niklas.sarbasini@gmail.com>
2026-04-16 13:45:05 +0200

    FieldSpec added more tests

--- src/test/scala/de/konstopoly/model/FieldSpec.scala
+++ src/test/scala/de/konstopoly/model/FieldSpec.scala
@@ -1,13 +1,13 @@
 package de.konstopoly.model
 
-import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import de.konstopoly.model.fields.PropertyField
+import org.scalatest.wordspec.AnyWordSpec
 
 // Tests für den Field-Trait und seine Unterklassen.
 // Prüft: landOn-Verhalten für PropertyField, TaxField,
 // ChanceField und GoField jeweils mit den erwarteten Effekten.
-class FieldSpec extends AnyFlatSpec with Matchers:
+class FieldSpec extends AnyWordSpec with Matchers:
 
   val badstrasse: PropertyField = PropertyField(
     name       = "Badstraße",
@@ -17,33 +17,30 @@ class FieldSpec extends AnyFlatSpec with Matchers:
     rents      = Vector(2, 10, 30, 90, 160, 250)
   )
 
-  "PropertyField" should "am Anfang keinen Besitzer haben" in {
-    badstrasse.isUnowned shouldBe true
-  }
-
-  it should "nach dem Kauf einen Besitzer haben" in {
-    val gekauft = badstrasse.buyBy("Alice")
-    gekauft.owner shouldBe Some("Alice")
-  }
-
-  it should "die Grundmiete ohne Häuser zurückgeben" in {
-    badstrasse.currentRent shouldBe 2
-  }
-
-  it should "nach dem Bau eines Hauses die richtige Miete berechnen" in {
-    val mitHaus = badstrasse.buildHouse
-    mitHaus.currentRent shouldBe 10
-  }
-
-  it should "nach 4 Häusern ein Hotel bauen können" in {
-    val mitHotel = badstrasse
-      .buildHouse.buildHouse.buildHouse.buildHouse
-      .buildHotel
-    mitHotel.hasHotel shouldBe true
-    mitHotel.currentRent shouldBe 250
-  }
-
-  it should "bei Hypothek keine Miete verlangen" in {
-    val mitHypothek = badstrasse.mortgage
-    mitHypothek.currentRent shouldBe 0
+  "PropertyField" should {
+    "leer sein und keinen Besitzer haben" in {
+      badstrasse.owner shouldBe None
+    }
+    "nach dem Kauf einen Besitzer haben" in {
+      val gekauft = badstrasse.buyBy("Alice")
+      gekauft.owner shouldBe Some("Alice")
+    }
+    "die Grundmiete ohne Häuser zurückgeben" in {
+      badstrasse.currentRent shouldBe 2
+    }
+    "nach dem Bau eines Hauses die richtige Miete berechnen" in {
+      val mitHaus = badstrasse.buildHouse
+      mitHaus.currentRent shouldBe 10
+    }
+    "nach 4 Häusern ein Hotel bauen können" in {
+      val mitHotel = badstrasse
+        .buildHouse.buildHouse.buildHouse.buildHouse
+        .buildHotel
+      mitHotel.hasHotel shouldBe true
+      mitHotel.currentRent shouldBe 250
+    }
+    "bei Hypothek keine Miete verlangen" in {
+      val mitHypothek = badstrasse.mortgage
+      mitHypothek.currentRent shouldBe 0
+    }
   }
