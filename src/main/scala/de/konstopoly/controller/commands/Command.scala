f291ddc0befee97bdbd5cb6fd25f7cc8cb197ff3
niklassarbasini <niklas.sarbasini@gmail.com>
2026-06-05 01:31:18 +0200

    fix(ci): commit fehlende commands-Package (Command, CommandSpec)
    GameController importiert de.konstopoly.controller.commands.*, aber die
    Dateien waren nur untracked und nie gepusht. Dadurch schlug der CI-Build
    mit "value commands is not a member of de.konstopoly.controller" fehl.

---
+++ src/main/scala/de/konstopoly/controller/commands/Command.scala
@@ -0,0 +1,34 @@
+package de.konstopoly.controller.commands
+
+import de.konstopoly.model.GameState
+
+trait Command:
+  def doStep(): Unit
+  def undoStep(): Unit
+
+class UndoManager:
+  private var undoStack: List[Command] = Nil
+  private var redoStack: List[Command] = Nil
+
+  def execute(cmd: Command): Unit =
+    cmd.doStep()
+    undoStack = cmd :: undoStack
+    redoStack = Nil
+
+  def undo(): Boolean =
+    undoStack match
+      case cmd :: rest =>
+        cmd.undoStep()
+        undoStack = rest
+        redoStack = cmd :: redoStack
+        true
+      case Nil => false
+
+  def redo(): Boolean =
+    redoStack match
+      case cmd :: rest =>
+        cmd.doStep()
+        redoStack = rest
+        undoStack = cmd :: undoStack
+        true
+      case Nil => false
