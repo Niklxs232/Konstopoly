1188d7bbaff004b223babe74104c3da62fc92bb8
niklassarbasini <niklas.sarbasini@gmail.com>
2026-06-26 11:54:53 +0200

    Task 10 and a bit of 11

--- src/main/scala/de/konstopoly/Main.scala
+++ src/main/scala/de/konstopoly/Main.scala
@@ -6,9 +6,7 @@ import de.konstopoly.module.KonstopolyModule
 import de.konstopoly.view.{GUI, TUI}
 
 @main def main(): Unit =
-  val injector = Guice.createInjector(new KonstopolyModule)
 
-  val controller = injector.getInstance(classOf[ControllerInterface])
 
   val gui = new GUI(controller)
   val tui = new TUI(controller)
